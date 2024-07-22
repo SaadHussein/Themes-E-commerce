8b756889e5a5964b665a9753d0f0f37eeefaca92
SaadHussein <sa3dhusseein@gmail.com>
2024-07-22 19:18:49 +0300

    add more logic security

--- src/components/MainPage/CategoriesAndProducts/FreeProducts.tsx
+++ src/components/MainPage/CategoriesAndProducts/FreeProducts.tsx
@@ -27,19 +27,29 @@ const FreeProducts = () => {
 		getFreeProducts();
 	}, [token]);
 	return (
-		<div className="grid grid-cols-3 gap-8 max-[1250px]:grid-cols-2 max-[850px]:grid-cols-1 pt-8">
-			{freeProducts.map((product) => (
-				<ProductItem
-					price={product.price}
-					name={product.name}
-					category_id={product.category_id}
-					description={product.description}
-					id={product.id}
-					images={product.images}
-					key={product.category_id + product.id}
-				/>
-			))}
-		</div>
+		<>
+			{token === "" ? (
+				<div className="mt-20 mb-10">
+					<p className="text-[24px] font-semibold">
+						You Should Login First To See Products
+					</p>
+				</div>
+			) : (
+				<div className="grid grid-cols-3 gap-8 max-[1250px]:grid-cols-2 max-[850px]:grid-cols-1 pt-8">
+					{freeProducts.map((product) => (
+						<ProductItem
+							price={product.price}
+							name={product.name}
+							category_id={product.category_id}
+							description={product.description}
+							id={product.id}
+							images={product.images}
+							key={product.category_id + product.id}
+						/>
+					))}
+				</div>
+			)}
+		</>
 	);
 };
 
